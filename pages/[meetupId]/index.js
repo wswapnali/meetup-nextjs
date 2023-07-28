72961b4afc9b45d6384f0715f366988e1658f4c0
Swapnali Waghunde <swapnaliwaghunde@Swapnalis-MacBook-Pro.local>
2023-07-28 17:52:37 +0800

    fixed fallback

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -31,7 +31,7 @@ export async function getStaticPaths () {
     const meetups = await meetupCollection.find({}, { _id: 1 }).toArray();
     client.close();
     return {
-        fallback: false, 
+        fallback: 'blocking', 
         paths: meetups.map((meetup) => ({
             params: {meetupId: meetup._id.toString()}
         }))
